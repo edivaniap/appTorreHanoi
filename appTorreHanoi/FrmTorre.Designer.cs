4b5dc218e9734b987982928abbbe6a2d5f05b3df
Edivânia Pontes <edivaniap@outlook.com>
2015-05-21 02:20:01 -0300

    game

---
+++ appTorreHanoi/FrmTorre.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace appTorreHanoi
+{
+    partial class FrmTorre
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.disc3 = new System.Windows.Forms.Panel();
+            this.disc2 = new System.Windows.Forms.Panel();
+            this.disc1 = new System.Windows.Forms.Panel();
+            this.p1 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.p2 = new System.Windows.Forms.Panel();
+            this.p3 = new System.Windows.Forms.Panel();
+            this.panel12 = new System.Windows.Forms.Panel();
+            this.panel13 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.panel1.Location = new System.Drawing.Point(12, 249);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 25);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.panel2.Location = new System.Drawing.Point(157, 87);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(12, 162);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.panel3.Location = new System.Drawing.Point(406, 87);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(12, 162);
+            this.panel3.TabIndex = 2;
+            this.panel3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel3_MouseClick);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.panel4.Location = new System.Drawing.Point(656, 87);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(12, 162);
+            this.panel4.TabIndex = 3;
+            // 
+            // disc3
+            // 
+            this.disc3.BackColor = System.Drawing.Color.MidnightBlue;
+            this.disc3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.disc3.Location = new System.Drawing.Point(62, 217);
+            this.disc3.Name = "disc3";
+            this.disc3.Size = new System.Drawing.Size(200, 30);
+            this.disc3.TabIndex = 4;
+            // 
+            // disc2
+            // 
+            this.disc2.BackColor = System.Drawing.Color.DarkMagenta;
+            this.disc2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.disc2.Location = new System.Drawing.Point(82, 185);
+            this.disc2.Name = "disc2";
+            this.disc2.Size = new System.Drawing.Size(160, 30);
+            this.disc2.TabIndex = 8;
+            // 
+            // disc1
+            // 
+            this.disc1.BackColor = System.Drawing.Color.Crimson;
+            this.disc1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.disc1.Location = new System.Drawing.Point(102, 153);
+            this.disc1.Name = "disc1";
+            this.disc1.Size = new System.Drawing.Size(120, 30);
+            this.disc1.TabIndex = 9;
+            this.disc1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.disc1_MouseClick);
+            // 
+            // p1
+            // 
+            this.p1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.p1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.p1.Location = new System.Drawing.Point(292, 74);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(200, 30);
+            this.p1.TabIndex = 5;
+            this.p1.Visible = false;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel8.Location = new System.Drawing.Point(562, 217);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(200, 30);
+            this.panel8.TabIndex = 5;
+            this.panel8.Visible = false;
+            // 
+            // p2
+            // 
+            this.p2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.p2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.p2.Location = new System.Drawing.Point(332, 185);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(160, 30);
+            this.p2.TabIndex = 9;
+            this.p2.Visible = false;
+            // 
+            // p3
+            // 
+            this.p3.BackColor = System.Drawing.Color.MidnightBlue;
+            this.p3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.p3.Location = new System.Drawing.Point(352, 217);
+            this.p3.Name = "p3";
+            this.p3.Size = new System.Drawing.Size(120, 30);
+            this.p3.TabIndex = 10;
+            this.p3.Visible = false;
+            // 
+            // panel12
+            // 
+            this.panel12.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel12.Location = new System.Drawing.Point(582, 185);
+            this.panel12.Name = "panel12";
+            this.panel12.Size = new System.Drawing.Size(160, 30);
+            this.panel12.TabIndex = 10;
+            this.panel12.Visible = false;
+            // 
+            // panel13
+            // 
+            this.panel13.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel13.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel13.Location = new System.Drawing.Point(603, 153);
+            this.panel13.Name = "panel13";
+            this.panel13.Size = new System.Drawing.Size(120, 30);
+            this.panel13.TabIndex = 11;
+            this.panel13.Visible = false;
+            // 
+            // FrmTorre
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 301);
+            this.Controls.Add(this.panel13);
+            this.Controls.Add(this.panel12);
+            this.Controls.Add(this.p3);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.p1);
+            this.Controls.Add(this.disc1);
+            this.Controls.Add(this.disc2);
+            this.Controls.Add(this.disc3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmTorre";
+            this.Text = "FrmTorre";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel disc3;
+        private System.Windows.Forms.Panel disc2;
+        private System.Windows.Forms.Panel disc1;
+        private System.Windows.Forms.Panel p1;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel p2;
+        private System.Windows.Forms.Panel p3;
+        private System.Windows.Forms.Panel panel12;
+        private System.Windows.Forms.Panel panel13;
+    }
+}
---
+++ appTorreHanoi/FrmTorre.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace appTorreHanoi
+{
+    public partial class FrmTorre : Form
+    {
+        public FrmTorre()
+        {
+            InitializeComponent();
+        }
+
+        public bool cliqueiDisc()
+        {
+            if (disc1.BorderStyle == BorderStyle.Fixed3D || disc2.BorderStyle == BorderStyle.Fixed3D || disc1.BorderStyle == BorderStyle.Fixed3D)
+            return true;
+            else
+            return false;
+        }
+
+        private void disc1_MouseClick(object sender, MouseEventArgs e)
+        {
+            disc1.BorderStyle = BorderStyle.Fixed3D;
+        }
+
+        private void panel3_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (cliqueiDisc() == true)
+            {
+                disc1.Location = p3.Location;
+                disc1.BorderStyle = BorderStyle.FixedSingle;
+            }
+        }
+
+    }
+}
